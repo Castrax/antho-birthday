0ddea7456283dcb55fc63ba3e725f021e72940ca
Mathilde Robert <m.robert@skema.edu>
2020-04-11 19:58:12 +0200

    hp

--- app/javascript/components/mobile-menu-toggle.js
+++ app/javascript/components/mobile-menu-toggle.js
@@ -1,7 +1,9 @@
 const classToggle = () => {
   const nav = document.querySelector('.Navbar__Items');
-  nav.classList.toggle('Navbar__ToggleShow');
-  document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
+  if (nav) {
+    nav.classList.toggle('Navbar__ToggleShow');
+    document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
+  };
 };
 
 export { classToggle };
